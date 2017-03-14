89bc981bf348eb9e7ff9e3cb3b4dbc51258e18b3
allexxis <gabr.933@gmail.com>
2017-03-14 17:49:59 -0600

    Agregue todas las operaciones elementales

--- Algebra/src/Datos/Matriz.java
+++ Algebra/src/Datos/Matriz.java
@@ -75,52 +75,85 @@ public class Matriz {
         matriz = matriztmp;
     
     }
-    
-
+    //Operacion para multiplicar un numero por una fila de la matriz 
+    public ArrayList<Double> escalarPorFila(Double n,int fila){
+        ArrayList<Double> filatmp = new ArrayList<>();
+        for (int i = 0; i < matriz.get(fila).size(); i++) {
+            filatmp.add( matriz.get(fila).get(i)*n);
+            
+        }      
+        return  filatmp;
+    }
 
     //En este caso la suma de las filas es sin muplicacion al inicio Ejemplo [F1-->F2]
     public void sumaFila(int filaN,int filaM){ 
-        ArrayList<Double> filatmp = new ArrayList<>();
-        for (int i = 0; i < matriz.size(); i++) {
-            for (int j = 0; j < matriz.get(i).size(); j++) {
-                
-                
-            }
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, matriz.get(filaN).get(i)+matriz.get(filaN).get(i));
             
         }
 
     }
     //En este caso  la  filaN lleva multiplicacion al inicio Ejemplo: 3F1-->F2
     public void sumaFila(Double escalarN,int filaN ,int filaM){
-
-
+        ArrayList<Double> filatmp = escalarPorFila(escalarN, filaN);
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, filatmp.get(i)+matriz.get(filaM).get(i));
+            
+        }
     }
     //En este casola filaM lleva multiplicacion al inicio [Ejemplo F1-->3F2]
     public void sumaFila(int filaN,Double escalarM,int filaM){
 
+        ArrayList<Double> filatmp = escalarPorFila(escalarM, filaM);
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, matriz.get(filaN).get(i)+filatmp.get(i));
+            
+        }
     }
     
+    
     //En este caso filaN y filaM llevan multiplicacion al inicio [Ejemplo 4F1-->5F2]
-    public void sumaFila(Double escalarN,int filaN,Double escalarM,int filaM){
-
+    public void sumaFila(Double escalarN,int filaN,Double escalarM,int filaM){  
+        ArrayList<Double> filatmp1 = escalarPorFila(escalarN, filaN);
+        ArrayList<Double> filatmp2 = escalarPorFila(escalarM, filaM);
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, filatmp2.get(i)+filatmp1.get(i));
+            
+        }
     }
         //En este caso la suma de las filas es sin muplicacion al inicio Ejemplo [F1-->F2]
     public void restaFila(int filaN,int filaM){ 
-
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, matriz.get(filaN).get(i)-matriz.get(filaN).get(i));
+            
+        }
 
     }
     //En este caso  la  filaN lleva multiplicacion al inicio Ejemplo: 3F1-->F2
     public void restaFila(Double escalarN,int filaN ,int filaM){
-
+        ArrayList<Double> filatmp = escalarPorFila(escalarN, filaN);
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, filatmp.get(i)-matriz.get(filaM).get(i));
+            
+        }
 
     }
     //En este casola filaM lleva multiplicacion al inicio [Ejemplo F1-->3F2]
     public void restaFila(int filaN,Double escalarM,int filaM){
-
+        ArrayList<Double> filatmp = escalarPorFila(escalarM, filaM);
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, matriz.get(filaN).get(i)-filatmp.get(i));
+            
+        }
     }
     
     //En este caso filaN y filaM llevan multiplicacion al inicio [Ejemplo 4F1-->5F2]
     public void restaFila(Double escalarN,int filaN,Double escalarM,int filaM){
-
+        ArrayList<Double> filatmp1 = escalarPorFila(escalarN, filaN);
+        ArrayList<Double> filatmp2 = escalarPorFila(escalarM, filaM);
+        for (int i = 0; i < matriz.get(filaN).size(); i++) {
+            matriz.get(filaN).set(i, filatmp2.get(i)-filatmp1.get(i));
+            
+        }
     }
 }
