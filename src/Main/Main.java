37730ae58d7ba8f5a0c410cf82e058dbe217c4a1
fabianmg <fmonge@users.noreply.github.com>
2017-04-09 15:10:42 -0600

    FIX MAIN

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -16,34 +16,20 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
-        Fraccion a = new Fraccion(555,5);
-        Fraccion b = new Fraccion(5,50);
-        Fraccion c = new Fraccion(1,1);
-        System.out.println("Operaciones con fracciones");        
-        c = c.operacion(a, b, '+');
-        System.err.println("-->"+c.toString());
-        c = c.operacion(a, b, '-');
-        System.err.println("-->"+c.toString());
-        c = c.operacion(a, b, '*');
-        System.err.println("-->"+c.toString());
-        c = c.operacion(a, b, '/');
-        System.err.println("-->"+c.toString());
-        
-        System.out.println("Operaciones con Matrice de fracciones");
-        
-        ArrayList<Integer> test0 = new ArrayList<Integer>();
-        ArrayList<ArrayList<Integer>> test1 = new ArrayList<ArrayList<Integer>>();
-        ArrayList<ArrayList<Fraccion>> test2 = new ArrayList<ArrayList<Fraccion>>();
-        test0.set(0, 1);
-        test1.set(0, test0);
-                
-        Matriz ma = new Matriz();
-        
-        //matriz c = new Matriz(Fraccion(1,1);
-        
-        
-        //Principal p = new Principal();
-        //p.setVisible(true);
+        //Fraccion a = new Fraccion(555,5);
+        //Fraccion b = new Fraccion(5,50);
+        //Fraccion c = new Fraccion(1,1);
+        //System.out.println("Operaciones con fracciones");        
+        //c = c.operacion(a, b, '+');
+        //System.err.println("-->"+c.toString());
+        //c = c.operacion(a, b, '-');
+        //System.err.println("-->"+c.toString());
+        //c = c.operacion(a, b, '*');
+        //System.err.println("-->"+c.toString());
+        //c = c.operacion(a, b, '/');
+        //System.err.println("-->"+c.toString());
+        Principal p = new Principal();
+        p.setVisible(true);
         
     }
     
